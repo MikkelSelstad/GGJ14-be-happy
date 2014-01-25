a993eb4602919223f4b2f9b8d3ebe1ef72641d5f
Sirslask <sirslask@hotmail.com>
2014-01-25 04:04:48 +0100

    Fixed happiness calculator, added generic happenings script

--- Happiness.cs
+++ Happiness.cs
@@ -7,7 +7,7 @@ public class Happiness : MonoBehaviour {
 	public int minhappy = 0; 
 	public int happylvl = 10;
 	float tick = 1;
-
+//	int i = 4;
 
 	// Use this for initialization
 	void Start () 
@@ -17,22 +17,45 @@ public class Happiness : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
-		//print (tick);
+	void Update () {
+
+		print (tick);
+
 		tick -= Time.deltaTime;
 		if(tick <= 0)
 		{
 			tick = 1;
 			if(happylvl > minhappy)
-			happylvl --;
+				happylvl --;
 		}	
-	
-		if(happylvl = minhappy)
-			Time.timeScale = 0.5f
+
+
+		if (happylvl == minhappy) 
+		{
+			Time.timeScale = 0.2f;
+			print("time slow");
+		}
+
+		if (happylvl >= maxhappy)
+		{
+			happylvl = 100;
+		}
+
+		if (happylvl < minhappy) 
+		{
+			happylvl = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			if (happylvl <= maxhappy--)
+			{
+				happylvl = happylvl +20;			
+			}
+		}
 
 
 
 	}
-
+	
 }
