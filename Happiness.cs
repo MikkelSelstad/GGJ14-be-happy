6e7181199b1a31e0c8587bab500afffaa89ada19
Sirslask <sirslask@hotmail.com>
2014-01-25 02:07:31 +0100

    Added happiness calculator
    still sip

---
+++ Happiness.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Happiness : MonoBehaviour {
+
+	public int maxhappy = 100; 
+	public int minhappy = 0; 
+	public float happylvl = 10;
+	float tick = 10;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		print (tick);
+
+		tick -= Time.deltaTime;
+		if(tick <= 0)
+		{
+			tick = 10;
+			if(happylvl > minhappy)
+				happylvl --;
+		}	
+
+
+		if (Input.GetKey (KeyCode.Space))
+			happylvl =+ 20;
+
+	}
+
+}
